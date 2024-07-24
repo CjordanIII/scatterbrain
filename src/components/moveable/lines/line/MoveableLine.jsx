48e85e325c1640b88fbe73eaeab472f668c774de
Clarence Jordan <clarencejordan087@gmail.com>
2024-07-24 15:21:16 -0500

    add tracking

--- src/components/moveable/lines/line/MoveableLine.jsx
+++ src/components/moveable/lines/line/MoveableLine.jsx
@@ -1,13 +1,19 @@
 import { motion } from "framer-motion";
+import { useState } from "react";
 import Line from "./Line";
 
 const MoveableLine = ({ squareTracker }) => {
   //  tracker
+  const [moveable, setMoveable] = useState(true);
+
   function lineTracker(event, info) {
+    console.log(moveable);
     console.log(info.point.x, info.point.y);
   }
   // tap
+
   function onLIneStart(event, info) {
+    setMoveable((prev) => !prev);
     console.log(info.point.x, info.point.y);
   }
   return (
@@ -17,14 +23,17 @@ const MoveableLine = ({ squareTracker }) => {
       onDrag={lineTracker}
       onTapStart={onLIneStart}
       initial={{
-        x: 0,
-        y: 0,
+        x: moveable ? squareTracker[0] : 0,
+        y: moveable ? squareTracker[1] : 0,
       }}
       whileTap={{ scale: 0.8 }}
       whileHover={{ scale: 1.05 }}
+      animate={{ x: squareTracker[0] + 88, y: squareTracker[1] }}
       dragConstraints={{
-        top: 0,
-        left: 0,
+        top: squareTracker[1] / 2,
+        left: squareTracker[0] / 2,
+        right: squareTracker[0] / 2,
+        bottom: squareTracker[1] / 2,
       }}
     >
       <Line />
