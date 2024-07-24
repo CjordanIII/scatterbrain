ded8b0c0aca016758499b3405c970b3e36b98f91
Clarence Jordan <clarencejordan087@gmail.com>
2024-07-23 23:10:32 -0500

    add toggle in NavBar

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -7,9 +7,11 @@ const NavBar = () => {
   console.log(settingsToggle);
   return (
     <div className="fixed z-50 w-full h-16 max-w-lg -translate-x-1/2 bg-white border border-gray-200 rounded-full bottom-4 left-1/2 dark:bg-gray-700 dark:border-gray-600">
-      <div className="absolute bottom-16 left-44">
-        <Options />
-      </div>
+      {settingsToggle && (
+        <div className="absolute bottom-16 left-44">
+          <Options />
+        </div>
+      )}
       <div className="grid h-full max-w-lg grid-cols-5 mx-auto">
         <Btns type={"Home"} />
         <Btns type={"Wallet"} />
