3ca73211ac70390ec0e3e56d4ff3d42f1da81e3a
Clarence Jordan <clarencejordan087@gmail.com>
2024-07-23 21:56:04 -0500

    add btn and context

--- src/constraints/Btns.jsx
+++ src/constraints/Btns.jsx
@@ -1,9 +1,13 @@
 // eslint-disable-next-line react/prop-types
-const Btns = ({ type, setCount, count }) => {
+import { useContext } from "react";
+import { CountContext } from "../pages/Main";
+const Btns = ({ type }) => {
+  let [count, setCount] = useContext(CountContext);
+
   const addNum = () => {
-    setCount(count++);
-    console.log(count);
+    setCount(++count);
   };
+
   if (type === "Home") {
     return (
       <>
@@ -75,21 +79,25 @@ const Btns = ({ type, setCount, count }) => {
             onClick={addNum}
             className="inline-flex items-center justify-center w-10 h-10 font-medium bg-blue-600 rounded-full hover:bg-blue-700 group focus:ring-4 focus:ring-blue-300 focus:outline-none dark:focus:ring-blue-800"
           >
-            <svg
-              className="w-4 h-4 text-white"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 18 18"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M9 1v16M1 9h16"
-              />
-            </svg>
+            {count === 0 ? (
+              <svg
+                className="w-4 h-4 text-white"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 18 18"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M9 1v16M1 9h16"
+                />
+              </svg>
+            ) : (
+              <span className="text-gray-200">{count}</span>
+            )}
             <span className="sr-only">{type}</span>
           </button>
         </div>
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,24 +1,27 @@
-import { useState } from "react";
+import { createContext, useState } from "react";
 import Moveable from "../components/moveable/Moveable.jsx";
 import NavBar from "../components/navbar/NavBar";
 import { squareCount } from "../constraints/amount.js";
-
+export const CountContext = createContext();
 const Main = () => {
-  // Assuming squareCount(5) has some necessary side effect or future use
+  // count
   const [count, setCount] = useState(0);
+  // range array
   let square = squareCount(count);
 
   let components = [];
-
-  for (let i = 0; i <= square.length; i++) {
+  // for length of itterater of arr
+  for (let i = 0; i < square.length; i++) {
     components.push(<Moveable key={i} num={i} />);
   }
 
   return (
-    <div className="bg-lines bg-dots">
-      <NavBar setCount={setCount} count={count} />
-      {components.map((component) => component)}
-    </div>
+    <CountContext.Provider value={[count, setCount]}>
+      <div className="bg-lines bg-dots">
+        <NavBar setCount={setCount} />
+        {components.map((component) => component)}
+      </div>
+    </CountContext.Provider>
   );
 };
 
